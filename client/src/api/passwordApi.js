c06c71b8a537fad1d2f587d5de98b84ca8444b8a
Sridhar N <110921859+sridhar10464@users.noreply.github.com>
2023-06-08 22:05:42 +0530

    Update passwordApi.js

--- client/src/api/passwordApi.js
+++ client/src/api/passwordApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const rootUrl = "http://localhost:3001/v1/";
+const rootUrl = "https://crm-system-ikhw.onrender.com/v1/";
 const otpReqUrl = rootUrl + "user/reset-password";
 const updatePassUrl = rootUrl + "user/reset-password";
 
